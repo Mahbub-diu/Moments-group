d8a875256b246fddba968b639459d587848db0cd
Mahbub-diu <mahbub.mediasoft@gmail.com>
2022-01-30 17:45:56 +0600

    update

---
+++ Assets/js/custom.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(){
+ 
+  document.querySelectorAll('.dropdown-menu').forEach(function(element){
+    element.addEventListener('click', function (e) {
+      e.stopPropagation();
+    });
+  })
+}); 
+
+// sticky navbar 
+document.addEventListener("DOMContentLoaded", function(){
+      
+  window.addEventListener('scroll', function() {
+       
+    if (window.scrollY > 200) {
+      document.getElementById('navbar_top').classList.add('fixed-top');
+  
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector('.navbar').offsetHeight;
+      document.body.style.paddingTop = navbar_height + 'px';
+    } else {
+       document.getElementById('navbar_top').classList.remove('fixed-top');
+       // remove padding top from body
+      document.body.style.paddingTop = '0';
+    } 
+  });
+}); 
