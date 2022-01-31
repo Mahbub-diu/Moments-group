5478b02c76f2cbe09657eb6b8a91ccf2b2fe01c3
Mahbub-diu <mahbub.mediasoft@gmail.com>
2022-01-31 18:34:10 +0600

    update

--- Assets/js/custom.js
+++ Assets/js/custom.js
@@ -25,3 +25,59 @@ document.addEventListener("DOMContentLoaded", function(){
     } 
   });
 }); 
+
+/****Dropdown toggle ****/
+$(".selectbox-title").click(function() {
+  $(".selectbox-content").slideToggle("slow");
+});
+
+/***Dynamic Checkbox generate***/
+var initial = 0;
+var maxlength = 100000;
+for (i = 0; i <= 10; i++) {
+  $('.ul-list').append('<li><input class="checkbox-custom" name="checkAll" type="checkbox" id="test' + i + '" /><label for="test' + i + '" class="checkbox-custom-label checkbox">Checkbox '+ i +'</label></li>');
+}
+
+/***Add Checkboxes on load more button***/
+function buttonclick() {
+        for (var i = initial; i < initial + 500; i++) {
+            $('.ul-list').append('<li><input class="checkbox-custom" name="checkAll" type="checkbox" id="test' + i + '" /><label for="test' + i + '" class="checkbox-custom-label checkbox">Checkbox</label></li>');
+        }
+        initial = initial + 500;
+    }
+
+$('#box').keyup(function () {
+        var valThis = $(this).val().toLowerCase();
+        if (valThis == "") {
+            $('.ul-list > li').show();
+        } else {
+            $('.ul-list > li').each(function () {
+                var text = $(this).text().toLowerCase();
+                (text.indexOf(valThis) >= 0) ? $(this).show() : $(this).hide();
+            });
+        };
+    });
+$('.fa-close').click(function () {
+        // Selects all elements of type text and clear all
+        $('#box').val('');
+    });
+
+/***Count Length of checkbox***/
+var $checkboxes = $('.ul-list input[type="checkbox"]');
+
+$checkboxes.change(function() {
+  var countCheckedCheckboxes = $checkboxes.filter(':checked').length;
+  $('#edit-count-checked-checkboxes').text(countCheckedCheckboxes);
+});
+
+if (countCheckedCheckboxes > 0) {
+            $('.plzselect').css('display', 'none');
+            $('#edit-count-checked-checkboxes').css('display', 'inline-block');
+            $('.selected').css('display', 'inline-block');
+        }
+        else {
+            $('.plzselect').css('display', 'block');
+            $('#edit-count-checked-checkboxes').css('display', 'none');
+            $('.selected').css('display', 'none');
+        }
+
